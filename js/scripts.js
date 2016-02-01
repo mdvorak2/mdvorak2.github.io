1646ea0278a74f5011a1197fd62104c46fb5be7d
mdvorak2 <mardvorak2@gmail.com>
2016-02-01 19:52:34 +0100

    1.2

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,31 @@ $(function(){
       $('.navigation').css('opacity', '1');
     },1950)
 
-
   }
 
   setTimeout(minimaze, 2000);
 
 
-})
+  $(window).scroll(function(){
+
+    var wScroll = $(this).scrollTop(),
+        wHeight = $(window).height();
+
+    console.log();
+
+    if(wScroll > $('.projects').offset().top - (wHeight/1.25)){
+      $('.projects .project').each(function(i){
+        setTimeout(function() {
+          $('.projects .project').eq(i).addClass('is-showing');
+        }, 250*(i+1));
+
+
+      });
+
+
+    }
+
+  });
+
+
+}) /* doc.rdy */
