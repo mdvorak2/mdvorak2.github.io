3fa0646169f2780813c7cc1ca29f07155e940b54
mdvorak2 <mardvorak2@gmail.com>
2016-02-04 19:24:32 +0100

    completely redesigned

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,75 @@
 $(function(){
 
 
-  function minimaze(){
-    $('#logo-section').css('height','0');
-    // $('#logo-section .logo').css('opacity', '0');
-    $('#logo-section h1').css('opacity', '0');
-    setTimeout(function(){
-      $('.navigation').css('opacity', '1');
-    },1950)
 
-  }
+  setTimeout(function(){
+        $('.intro h1').addClass('show');
+  },500)
 
-  setTimeout(minimaze, 2000);
+  setTimeout(function(){
+        $('.intro p').addClass('show');
+  },1000)
+
+
+  setTimeout(function(){
+        $('.mcbkair').addClass('show');
+  },1500)
+
+  setTimeout(function(){
+      $('.phone').addClass('show');
+  },1900)
+
+
+
 
 
   $(window).scroll(function(){
 
     var wScroll = $(this).scrollTop(),
-        wHeight = $(window).height();
-
-    console.log();
-
-    if(wScroll > $('.projects').offset().top - (wHeight/1.25)){
-      $('.projects .project').each(function(i){
-        setTimeout(function() {
-          $('.projects .project').eq(i).addClass('is-showing');
-        }, 250*(i+1));
+        wHeight = $(window).height(),
+        navbar = $('#navbar').offset().top;
 
 
-      });
+function stickNavbar(){
+  if(wScroll > navbar){
+    $('#navbar2').removeClass('hidden');
+    $('#navbar').addClass('hidden');
+  // }else{
+  //   $('#navbar2').addClass('hidden');
+  //   $('#navbar').removeClass('hidden');
+}
+}
+
+function show(element){
+  if(( element.offset().top-wScroll) <= (wHeight/1.5) ) {
+    element.addClass('show');
+  }
+
+}
 
 
-    }
+show( $('.about-img') );
 
+
+
+
+if(( $('.portfolio').offset().top-wScroll) <= (wHeight/1.55) ) {
+  $('.project a').each(function(i){
+    setTimeout(function() {
+      $('.project a').eq(i).addClass('show');
+    }, 250*(i+1));
+
+
+  });
+}
+
+
+
+
+
+
+
+stickNavbar();
   });
 
 
